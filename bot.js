e183c4870253a4c90442a117a7d185aee9366b61
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-13 09:32:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@
 const client = new Discord.Client();
 
 client.on('message', message => {
-if (message.content === `e1`) {
+if (message.content === `!start`) {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 10000000; x++) {
+      for(let x = 0; x < 1000000; x++) {
         message.channel.send(`i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits`)
           .then(m => {
             count++;
