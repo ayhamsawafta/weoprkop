46c14350fba4ff245ffd22284988624a4f9a3c91
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-13 09:21:39 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+ const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', message => {
+if (message.content === `e1`) {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 10000000; x++) {
+        message.channel.send(`i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.login("NDY0MDQ4NDg0MzI1MjYxMzIz.DsXoGA.qHQaeWZLkaXuRZnUMXOJGnOxw1I");
