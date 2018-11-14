028214629ea08d2217a67d86f0ec0b9db0825a05
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-14 07:18:02 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 
 client.on('message', message => {
-if (message.content === `!start`) {
+if (message.content === `!start5`) {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 1000000; x++) {
